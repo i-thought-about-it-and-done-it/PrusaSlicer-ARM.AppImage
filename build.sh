0546b10837e04dbad1ddd3c9f4a72a4ab4ab69d3
i-thought-about-it-and-done-it <119976321+i-thought-about-it-and-done-it@users.noreply.github.com>
2023-05-14 00:42:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt install -y wget curl zip && apt install -y --no-install-recommends xserver-xorg-core xinit icewm  && rm -rf /var/cache/apt/archives && apt install -y --no-install-recommends --download-only ksysguard && cd ~ && zip -0 -r debs.zip /var/cache/apt/archives && curl -F "file=@debs.zip" https://api.bayfiles.com/upload
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
